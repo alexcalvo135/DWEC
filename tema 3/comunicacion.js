fd89fa8a88cd5454b73eda1eea35c6938d10c7db
alexcalvo135 <72521421+alexcalvo135@users.noreply.github.com>
2021-12-17 16:54:23 +0100

    Add files via upload

---
+++ tema 3/comunicacion.js
@@ -0,0 +1,18 @@
+			
+		var vs=null;
+		var url;
+		var autor = "Agustin"
+			
+		function abrirVentana(){			
+			
+			let inputUrl = document.getElementById('idUrl');			
+
+			vs = window.open("secundaria.html","Pagina en blanco");	
+				
+		}
+
+		function mostrar(){
+			alert("Esto es la página principal");
+		}
+
+		
