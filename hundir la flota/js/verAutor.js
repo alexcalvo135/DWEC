f0a3f15e1c90fa5fb1f5bd3594efe4523950013f
alexcalvo135 <72521421+alexcalvo135@users.noreply.github.com>
2021-12-17 16:59:15 +0100

    Add files via upload

---
+++ hundir la flota/js/verAutor.js
@@ -0,0 +1,5 @@
+function abrirAutor(){
+      
+      vs = window.open("html/formAutor.html","dr", "width=600,height=500");
+      
+}
---
+++ hundir la flota/js/verDemo.js
@@ -0,0 +1,5 @@
+function abrirVentana(){
+      
+      vs = window.open("html/tablero.html","Tablero", "width=600,height=320");
+      
+}
